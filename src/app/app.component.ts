121b11a738fd4a69a7b1ff33a48550ed3498348b
eioannidis <docermis@gmail.com>
2018-07-30 23:47:22 +0300

    small toying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my First Solo Web App';
+	title = 'my First Solo Web App';
+
+	onClick() {
+		if (true) {
+			console.log('Hello World!');
+		}
+	}
+
 }
